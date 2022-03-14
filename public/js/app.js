41175e86f092b08b562a76a8abe59975a4553fd0
Graham Morison <rmorison3@gmail.com>
2022-03-14 10:10:16 -0400

    Set up app to be hosted on Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-console.log('Client side javascript file')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'From JS'
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -15,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading your location weather data'
   messageTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
